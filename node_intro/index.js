ffcfca9f70498df4cbf91f8b6f4bb1de5247f583
Jacob Williams <jjwill@hotmail.co.uk>
2019-02-04 13:48:16 +0000

    index.js update (not fully fixed)

--- node_intro/index.js
+++ node_intro/index.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res) {
 	res.send('Hello, World!');
 });
 
+app.post('/', function(req, res) {
+	res.statusCode(status.METHOD_NOT_ALLOWED);
+});
+
 var port = 3000;
 app.listen(port,function() {
 	console.log('Listening on port ' + port);
